d3707cc605d6bad3e6492de436046e9736a561e4
danb31 <daniel.bell96@hotmail.co.uk>
2024-11-13 13:45:44 +0000

    remove cached data update

--- dags/scripts/utils/os_utils.py
+++ dags/scripts/utils/os_utils.py
@@ -2,38 +2,8 @@ import os
 import shutil
 from datetime import datetime, timezone
 import re
-from dateutil import parser
+import stat
 
-
-# def remove_cached_data(base_dir, days_to_keep):
-#     DIR_NAME_PATTERN = re.compile(r'(.+)__([0-9T:+-]+)')
-#
-#     # Get the current time in UTC (timezone-aware)
-#     now = datetime.now(timezone.utc)
-#
-#     for root, dirs, files in os.walk(base_dir):
-#         for dir_name in dirs:
-#
-#             if not any(s in dir_name for s in ['manual__', 'scheduled__', 'dataset_triggered__', 'backfill__']):
-#                 continue
-#
-#             match = DIR_NAME_PATTERN.match(dir_name)
-#             if not match:
-#                 continue
-#
-#             # Extract timestamp from directory name
-#             dir_timestamp_str = match.group(2)
-#             dir_timestamp = parser.parse(dir_timestamp_str)
-#
-#             # If dir_timestamp is offset-naive (without timezone), make it UTC
-#             if dir_timestamp.tzinfo is None:
-#                 dir_timestamp = dir_timestamp.replace(tzinfo=timezone.utc)
-#
-#             # Check if the directory is older than the retention period
-#             if (now - dir_timestamp).days > days_to_keep:
-#                 dir_path = os.path.join(root, dir_name)
-#                 print(f"Removing directory: {dir_path}")
-#                 shutil.rmtree(dir_path)
 
 def remove_cached_data(base_dir, days_to_keep):
     # Updated regex pattern to match the new directory name format
@@ -60,5 +30,19 @@ def remove_cached_data(base_dir, days_to_keep):
             # Check if the directory is older than the retention period
             if (now - dir_timestamp).days > days_to_keep:
                 dir_path = os.path.join(root, dir_name)
+
+                print(f"Changing permissions for directory: {dir_path}")
+                # Change permissions for all files and subdirectories
+                for root_dir, subdirs, subfiles in os.walk(dir_path):
+                    for file_name in subfiles:
+                        file_path = os.path.join(root_dir, file_name)
+                        # Make file writable by the user
+                        os.chmod(file_path, stat.S_IWUSR | stat.S_IREAD)
+
+                    for sub_dir_name in subdirs:
+                        sub_dir_path = os.path.join(root_dir, sub_dir_name)
+                        # Writable and executable
+                        os.chmod(sub_dir_path, stat.S_IWUSR | stat.S_IREAD | stat.S_IEXEC)
+
                 print(f"Removing directory: {dir_path}")
                 shutil.rmtree(dir_path)
